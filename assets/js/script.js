9b2eb69571075218d0609e7bd5234641eff3cea0
Sparkle Party <lexi.slovik@gmail.com>
2021-11-17 15:42:26 -0800

    add task type to new items displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,23 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
-
+    //stop the browser from default refresh when form is submitted
     event.preventDefault();
-  
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    // div to hold task info and add to task item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // update list item with new input
+    listItemEl.appendChild(taskInfoEl);
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
   };
 
